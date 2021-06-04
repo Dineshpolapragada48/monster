cb98a70beab05235ff346e41573e5fbfe5789020
Dineshpolapragada48 <84314558+Dineshpolapragada48@users.noreply.github.com>
2021-06-04 20:51:06 +0530

    Update Hero.js

--- Hero.js
+++ Hero.js
@@ -1,30 +1,31 @@
-class Hero {
-  constructor(x,y,r)
+class hero {
+
+	constructor(x,y,w,h)
 	{
-		var options = { 
-			density: 1, 
-			frictionAir: 1
-		};
-		this.x=x;
-		this.y=y;
-		this.r=r;
-		this.image=loadImage("superhero1.png");
-		this.body=Bodies.circle(this.x, this.y, (this.r)/2, options);
-		World.add(world, this.body);
+		var options={
+            
+            density:1,
+            friction:0
+
+        }
+		this.x = x;
+		this.y = y;
+		this.w = w;
+		this.h = h;
+		this.body = Bodies.rectangle(x, y, w, h , options);
+		this.image = loadImage("Superhero-01.png");
+ 		World.add(world, this.body);
 
 	}
-	
-	display()
-	{
-			
-			var heroPos=this.body.position;		
+
+	display(){
+
+			var Pos = this.body.position;
 			push()
-			translate(heroPos.x, heroPos.y-100);
-			rectMode(CENTER)
-			fill(255,0,255)
 			imageMode(CENTER);
-			image(this.image, 0,0,this.r+150, this.r)
+			image(this.image,Pos.x, Pos.y,this.w, this.h);
 			pop()
 			
 	}
+
 }
