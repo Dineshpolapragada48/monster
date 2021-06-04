8bf9980a293c01ee41f99e92f04e6fcc7f0376fc
Dineshpolapragada48 <84314558+Dineshpolapragada48@users.noreply.github.com>
2021-06-04 20:51:55 +0530

    Update Box.js

--- Box.js
+++ Box.js
@@ -1,27 +1,24 @@
 class Box {
-  constructor(x, y, width, height) {
-    var options = {
-        'restitution':0.8,
-        'friction':1.0,
-        'density':20,
+    constructor(x,y,w,h){
+        var options={
+            restitution:0.8,
+            friction:1,
+            density:0.04
+
+        }
+        this.body=Bodies.rectangle(x,y,w,h,options)
+        this.x=x;
+        this.x=x;
+        this.w=w;
+        this.h=h;
+        World.add(world,this.body)
     }
-    this.body = Bodies.rectangle(x, y, width, height, options);
-    this.width = width;
-    this.height = height;
-    
-    World.add(world, this.body);
-  }
-  display(){
-    var pos =this.body.position;
-    var angle = this.body.angle;
-    push();
-    translate(pos.x, pos.y);
-    rotate(angle);
-    rectMode(CENTER);
-    strokeWeight(4);
-    stroke("black");
-    fill("red");
-    rect(0, 0, this.width, this.height);
-    pop();
-  }
+    display(){
+        var pos = this.body.position
+        push()
+        fill("red")
+        rectMode(CENTER)
+        rect(pos.x,pos.y,this.w,this.h)
+        pop()
+    }
 }
